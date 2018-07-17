c654c99f9ecafa3863fddffee2df9987c34d3b88
DanielHSwebs <40626839+DanielHSwebs@users.noreply.github.com>
2018-07-17 10:07:57 +0200

    made car stuff

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var sayMyName = function (name) {
+	alert('My name is: '+name)
+}
+
+
+var car = {
+	make: 'VW',
+	type: 'Polo',
+	color: 'blue',
+	isTurnedOn: false,
+	numberOfWheels: 4,
+	seats: [
+			'seat 1',
+			'seat 2',
+			'seat 3',
+			'seat 4'
+		 ],
+		 turnOn: function () {
+		 	this.isTurnedOn = true;
+		 },
+		 fly: function() {
+		 	alert('fly');
+		 },
+		 switchCar: function (IsOn) {
+			 console.log('turn car '+IsOn)
+			 	;
+			 if (IsOn == true) {
+			 	this.isTurnedOn = true;
+			 } else {
+				this.isTurnedOn = false;
+			 }
+		 }
+	};
+
+console.log  ('hello there friends!');
